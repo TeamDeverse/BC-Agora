b69dbadda5cf694de1b387408fdb100ffe798740
William Bowditch <bowditcw@bc.edu>
2015-11-01 14:47:58 -0500

    login and main page, Sunday lecture.

---
+++ BC-Agora/BC-Agora/AGOmainVC.swift
@@ -0,0 +1,25 @@
+//
+//  AGOmainVC.swift
+//  BC-Agora
+//
+//  Created by William Bowditch on 11/1/15.
+//  Copyright © 2015 Deverse. All rights reserved.
+//
+
+import UIKit
+
+class AGOmainVC: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("test1")
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    
+
+}
