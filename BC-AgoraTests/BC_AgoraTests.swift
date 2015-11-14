552bdf83d9ecf9d9c9faa612a8412a524887e21f
William Bowditch <bowditcw@bc.edu>
2015-11-13 21:36:29 -0500

    Initial Commit

---
+++ BC-AgoraTests/BC_AgoraTests.swift
@@ -0,0 +1,36 @@
+//
+//  BC_AgoraTests.swift
+//  BC-AgoraTests
+//
+//  Created by William Bowditch on 11/13/15.
+//  Copyright © 2015 William Bowditch. All rights reserved.
+//
+
+import XCTest
+@testable import BC_Agora
+
+class BC_AgoraTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
