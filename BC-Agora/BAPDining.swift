a309a25737b922fbddc5b450a5085d4cc854b4f1
William Bowditch <bowditcw@bc.edu>
2015-11-14 00:42:52 -0500

    friday night push, added icons.

---
+++ BC-Agora/BAPDining.swift
@@ -0,0 +1,74 @@
+//
+//  BAPMailbox.swift
+//  BC-Agora
+//
+//  Created by William Bowditch on 11/13/15.
+//  Copyright © 2015 William Bowditch. All rights reserved.
+//
+
+import UIKit
+
+class BAPDining: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    
+    
+    @IBOutlet var tableView: UITableView!
+    
+    let swiftBlogs = ["Dining Dollars:", "Flex Plan:", "Eagle Bucks:"]
+    let textCellIdentifier = "TextCell"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK:  UITextFieldDelegate Methods
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return swiftBlogs.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier(textCellIdentifier, forIndexPath: indexPath) as UITableViewCell
+        
+        let row = indexPath.row
+        cell.textLabel?.text = swiftBlogs[row]
+        
+        return cell
+    }
+    
+    // MARK:  UITableViewDelegate Methods
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        tableView.deselectRowAtIndexPath(indexPath, animated: true)
+        
+        let row = indexPath.row
+        print(swiftBlogs[row])
+    }
+    
+    
+    
+    /*
+    // MARK: - Navigation
+    
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+    // Get the new view controller using segue.destinationViewController.
+    // Pass the selected object to the new view controller.
+    }
+    */
+    
+}
