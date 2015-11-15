0af10b11ff45adf50e1e621d259e81b362f7586d
William Bowditch <bowditcw@bc.edu>
2015-11-15 15:39:31 -0500

    updated background color and back tab color.

--- BC-Agora/ViewController.swift
+++ BC-Agora/ViewController.swift
@@ -12,10 +12,14 @@ class ViewController: UIViewController {
 
     @IBOutlet var bcLogo: UIImageView!
     
+    @IBOutlet var user: UITextField!
+    @IBOutlet var password: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         bcLogo.image = UIImage(named: "BostonCollegeEagles.svg")
         print("hello")
+        //checkLogin()
         
          navigationController!.navigationBar.barTintColor = UIColor(red: 0.5, green: 0.1, blue: 0.1, alpha: 1)
         
@@ -23,6 +27,7 @@ class ViewController: UIViewController {
         
         self.navigationController!.navigationBar.titleTextAttributes = titleDict as? [String: AnyObject]
         
+        navigationController?.navigationBar.tintColor = UIColor.whiteColor()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -31,6 +36,24 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func pressLogin(sender: AnyObject) {
+        checkLogin()
+        
+    }
+    func loadUserDefault() {
+        //NSUser Default
+        //PS file
+        //run code before checkLogin code
+    }
+    func checkLogin(){
+        if self.user.text != "" && self.password.text != "" {
+            print("not empty")
+            self.performSegueWithIdentifier("loginSegue", sender: nil)
+            
+        }
+        print("working")
+        
+    }
 
 }
 
